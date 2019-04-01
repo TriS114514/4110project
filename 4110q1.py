79680824851ad4199fb4446663603073a349eb77
sais3720 <shohei.saito@uleth.ca>
2019-04-01 17:23:17 -0600

    add simple random path graph generator

--- 4110q1.py
+++ 4110q1.py
@@ -1,3 +1,38 @@
 #4110 final project assignment
-#Shohei Saito & 
+#Author: Shohei Saito & 
 #Topic 1: generate random path, illistrate puring process, compute p-center problem
+
+# p-center
+import osmnx as ox
+import networkx as nx
+import random
+from random import choice
+%matplotlib inline
+np.random.seed(1)
+
+#generate random path
+rnd = random.randint(8,16) # number of nodes
+G = nx.Graph()
+H=nx.path_graph(rnd)
+G.add_nodes_from(H)
+i = 0
+for n in G:
+    j = choice([k for k in range(0,rnd) if k not in [i]])
+    G.add_edge(i,j)
+    i+=1
+
+
+
+nd = G.number_of_nodes()
+ed = G.number_of_edges()
+ndc = G.nodes()
+edc = G.edges()
+print('number of nodes: ', nd)
+print('number of edges: ', ed)
+print(ndc)
+print(edc)
+
+#puring process
+
+
+#p-center
